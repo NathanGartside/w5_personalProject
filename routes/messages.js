391b910bfcf76f4619d627f31f4763b7751dad5a
Nathan Gartside <gar18014@byui.edu>
2023-11-04 12:56:38 -0600

    Added auth

--- routes/messages.js
+++ routes/messages.js
@@ -1,11 +1,12 @@
 const routes = require('express').Router();
 const baseController = require('../controllers/index')
 const validator = require('../utilites/validation')
+const { requiresAuth } = require('express-openid-connect');
 
-routes.post('/', validator.newMessage, validator.checkValidation, baseController.createNewMessage);
+routes.post('/', requiresAuth(), validator.newMessage, validator.checkValidation, baseController.createNewMessage);
 
-routes.get('/:receiver', baseController.getMessagesForUsername);
+routes.get('/:receiver', requiresAuth(), baseController.getMessagesForUsername);
 
-routes.delete('/:id', baseController.deleteMessage);
+routes.delete('/:id', requiresAuth(), baseController.deleteMessage);
 
 module.exports = routes;
--- server.js
+++ server.js
@@ -8,6 +8,25 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 const cors = require('cors');
 const createError = require('http-errors');
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'pzuVAM8sUysS4RFU9VbB2CAN3U6ZtowB',
+  issuerBaseURL: 'https://dev-xyl20jfosnr5hpox.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
 
 const port = process.env.PORT || 3000;
 
